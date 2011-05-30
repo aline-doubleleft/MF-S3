48a87bf6e413df742b94999f6bcd40d1e25af104
David Valdez <me@gnuget.org>
2011-05-30 04:18:37 -0500

    almost finish.
    Require a script for add one table into the database

--- main.php
+++ main.php
@@ -26,8 +26,6 @@ function mfs3_after_upload($file) {
     $s3->putObjectStream($s3_options['bucket'], $file_name, $file);
 }
 
-
-
 add_action('mf_before_delete_file','mfs3_before_delete');
 function mfs3_before_delete($file_name) {
   global $wpdb;
@@ -83,5 +81,106 @@ function  mfs3_save_image($field_meta_id,$name,$group_index,$field_index,$post_i
   if( $type == $FIELD_TYPES['image'])  {
     //Saving in the database a reference to this image
     $wpdb->query("INSERT INTO ".$wpdb->prefix."mfs3_images VALUES ('','".$s3_options['bucket']."','".$prefix.$value_field."')");
+  }
+}
+
+add_filter('mf_source_image', 'mfs3_source_image');
+function mfs3_source_image($image_source){
+  global $wpdb;
+
+  if (!$s3_options) $s3_options = get_option('tantan_wordpress_s3');
+
+  //getting the name of the file
+  preg_match('/wp\-content\/.+/',$image_source,$match);
+  $file = $match[0];
+
+  //checking if the image is in the CDN
+  $query = 'SELECT COUNT(1) FROM '.$wpdb->prefix.'mfs3_images WHERE bucket = "'.$s3_options['bucket'].'" AND image_path = "'.$file.'"';
+  $exists = $wpdb->get_var($query);
+
+  if($exists) {
+    $accessDomain = $s3_options['bucket'].'.s3.amazonaws.com';
+    return 'http://'.$accessDomain.'/'.$file;
+  }
+  return $image_source;
+}
+
+add_filter('mf_source_path_thumb_image', 'mfs3_source_thumb_image');
+function mfs3_source_thumb_image($params = array()){
+  global $wpdb;
+  
+  if (!$s3_options) $s3_options = get_option('tantan_wordpress_s3');
+
+  //getting the name of the file
+  preg_match('/wp\-content\/.+/',$params['thumb_url'],$match);
+  $file = $match[0];
+
+  //checking if the image is in the CDN
+  $query = 'SELECT COUNT(1) FROM '.$wpdb->prefix.'mfs3_images WHERE bucket = "'.$s3_options['bucket'].'" AND image_path = "'.$file.'"';
+  $exists = $wpdb->get_var($query);
+
+  if($exists) {
+    $accessDomain = $s3_options['bucket'].'.s3.amazonaws.com';
+    return array(true,'http://'.$accessDomain.'/'.$file);
+  }
+  return array(false,$params['thumb_url']);
+}
+
+add_action('mf_before_generate_thumb','before_generate_thumb'); 
+function before_generate_thumb($image_path){
+
+  //before to trying to download the file we are check if is in the local machine
+  if(file_exists($image_path)) {
+    return true;
+  }
+
+
+  if (!$s3_options) $s3_options = get_option('tantan_wordpress_s3');
+
+  preg_match('/(.+\/files_mf\/)(.+)/',$image_path,$match);
+  preg_match('/wp\-content\/.+/',$image_path,$match2);
+
+
+  $path = $match[1];
+  $image_name = $match[2];
+  $remote_file= $match2[0];
+    
+  $accessDomain = $s3_options['bucket'].'.s3.amazonaws.com';
+  $remote_file =  'http://'.$accessDomain.'/'.$remote_file;
+
+  $ch = curl_init();
+           
+  curl_setopt($ch, CURLOPT_URL, $remote_file);
+
+  $fp = fopen($image_path, 'w');
+
+  curl_setopt($ch, CURLOPT_FILE,$fp);
+  curl_exec($ch);
+  curl_close($ch);
+  fclose($fp);
+}
+
+
+add_action('mf_save_thumb_file','mfs3_save_thumb_file');
+function mfs3_save_thumb_file($filename){
+  global $wpdb,$FIELD_TYPES;
+
+  if($filename == "") {
+    return false;
+  }
+
+  //Loading the documentation of the tantan plugin
+  require_once(WP_PLUGIN_DIR.'/tantan-s3/wordpress-s3/lib.s3.php');
+  if (!$s3_options) $s3_options = get_option('tantan_wordpress_s3');
+
+  preg_match('/wp-content\/.+/',$filename,$match);
+
+  $file = $match[0];
+  $exists = $wpdb->get_var("SELECT count(1) FROM ".$wpdb->prefix."mfs3_images WHERE bucket = '".$s3_options['bucket']."' AND image_path = '".$file."'");
+
+
+  if(!$exists) {
+    //Saving in the database a reference to this image
+    $wpdb->query("INSERT INTO ".$wpdb->prefix."mfs3_images VALUES ('','".$s3_options['bucket']."','".$file."')");
   }
 }
